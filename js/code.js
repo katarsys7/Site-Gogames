11298ff36d1362155ff16ad2c3eaeae9891255ce
KirillBer <146697596+KirillBer@users.noreply.github.com>
2023-11-13 11:17:44 +0500

    11.13.2023

---
+++ js/code.js
@@ -0,0 +1,6 @@
+document.forms.login;
+<form type="login">
+    <input type="text" name="firstname" pattern="[a-z][A-Z]" value="Введите имя"></input>
+    <input type="text" name="secondname" pattern="[a-z][A-Z]" value="Введите фамилию"></input>
+    <input type="radio"></input>
+</form>
