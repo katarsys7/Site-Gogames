7287da3fe97d203c8ffc6da156c7d4e344786d37
KirillBer <146697596+KirillBer@users.noreply.github.com>
2023-11-20 10:42:54 +0500

    Add files via upload
    11.20.2023

--- js/code.js
+++ js/code.js
@@ -1,6 +1,1 @@
 document.forms.login;
-<form type="login">
-    <input type="text" name="firstname" pattern="[a-z][A-Z]" value="Введите имя"></input>
-    <input type="text" name="secondname" pattern="[a-z][A-Z]" value="Введите фамилию"></input>
-    <input type="radio"></input>
-</form>
