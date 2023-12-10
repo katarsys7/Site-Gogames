ea69b640461d8c24cdcf02f52cfeab00e6e42a20
KirillBer <146697596+KirillBer@users.noreply.github.com>
2023-12-10 20:49:39 +0500

    Add files via upload
    10.12.2023

--- js/code.js
+++ js/code.js
@@ -1,1 +1,35 @@
-document.forms.login;
+button = document.createElement('button');
+button.id = 'button';
+let rem = document.querySelector("#form");
+let q = 0;
+
+button.onclick=function(){
+    alert("btn");
+    if (q==0)
+    {
+        rem.remove();
+    }
+    else 
+    {
+        document.createElement("#form");
+    }
+}
+button.textContent='Feedback';
+document.body.appendChild(button);
+
+let s = 0;
+styleb = document.createElement('styleb');
+styleb.id = 'styleb';
+
+styleb.onclick=function(){
+    if (s==0)
+    {
+        rem.remove();
+    }
+    else 
+    {
+        document.createElement("#form");
+    }
+}
+button.textContent='Feedback';
+document.body.appendChild(button);
